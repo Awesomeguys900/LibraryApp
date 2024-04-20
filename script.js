ddde76e98a5aecc98beb19f56b4d7e48f1327a8e
Avi <awesomeguys900@gmail.com>
2024-04-20 11:16:55 +1000

    Using Prototypes to store book info, and updated CSS and animations,

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const myLibrary = [];
+const myLibrary = [new Book("1984", "George Orwell", "328", true)];
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.toggleRead = function () {
+    this.read = !this.read;
+  };
 }
 
 document
@@ -23,18 +26,59 @@ function addBookToLibrary() {
   );
   myLibrary.push(book);
 
-  const booklist = document.getElementById("bookList");
-
-  var card = document.createElement("div");
-  card.className = "card";
-  card.innerHTML = `<div>Title: ${book.title}</div><div>Author: ${book.author}</div><div>Pages: ${book.pages}</div><div>Read: ${book.read}</div>`;
-  booklist.appendChild(card);
+  togglePopup();
+  updatingBookList();
   document.getElementById("addBookForm").reset();
-  console.log(myLibrary);
-  togglePopup(); // Close popup after adding the book
+}
+
+function updatingBookList() {
+  const booklist = document.getElementById("bookList");
+  booklist.innerHTML = "";
+  myLibrary.forEach((book, index) => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+  <div class="title">${book.title}</div>
+  <div class="author">${book.author}</div>
+  <div class="pages">${book.pages} pages</div>
+`;
+
+    const readToggle = document.createElement("button");
+    readToggle.className = "readButton";
+    readToggle.textContent = book.read ? "Read" : "Not Read";
+    readToggle.classList.add(book.read ? "read" : "notRead"); //ternary operator which changes based on book read
+
+    readToggle.addEventListener("click", function () {
+      book.toggleRead();
+      readToggle.textContent = book.read ? "Read" : "Not Read";
+      if (book.read) {
+        readToggle.classList.remove("notRead");
+        readToggle.classList.add("read");
+      } else {
+        readToggle.classList.remove("read");
+        readToggle.classList.add("notRead");
+      }
+    });
+
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "deleteButton";
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", function () {
+      myLibrary.splice(index, 1);
+      updatingBookList();
+      console.log(myLibrary);
+    });
+
+    card.appendChild(readToggle);
+    card.appendChild(deleteButton);
+    booklist.appendChild(card);
+  });
 }
 
 function togglePopup() {
   const overlay = document.getElementById("popupOverlay");
   overlay.classList.toggle("show");
 }
+
+updatingBookList();
+document.getElementById("addBookForm").reset();
